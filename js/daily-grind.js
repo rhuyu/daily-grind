52a5c78aed142bfe6c31305f1cf5efc20c5a2499
Replit user <>
2023-06-05 22:12:39 +0000

    Templates

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -9,31 +9,49 @@ desc - a description of the coffee
 
 */
 
-let myDate= new Date();
+let myDate = new Date();
 
 let today = myDate.getDay();
+
+let coffee = "";
+
+function coffeeTemplate(coffee){
+  return `
+   <p>
+    		 <img src="${coffee.pic}" alt="${coffee.alt}" />
+            <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}</p>
+  `;
+}
 
 //today = 3; Testing other days
 
 switch(today){
-     case 2:
-          today = "Tuesday";
-     break;
+    case 1:
+          today = "Monday";
 
-     case 3:
-          today = "Wednesday";
-     break;
-
-     default: 
-         today = "Day is Unknown";
-}
-
-let coffee = {
+    coffee = {
     name: "Bubble Tea",
     pic: "images/bubble-tea.jpg",
     alt: "A Picture of a bubble tea",
     color: "pink",
-    day: "Wednesday",
+    day: "Monday",
     desc: `I love me some boba!`
+};
+    break;
+    
+    case 2:
+          today = "Tuesday";
+    break;
+
+    case 3:
+          today = "Wednesday";
+    break;
+
+    default: 
+         today = "Day is Unknown";
 }
-console.group(coffee) // Allows us to see the change in the console 
+
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
+
+console.log(coffee) // Allows us to see the change in the console 
+
